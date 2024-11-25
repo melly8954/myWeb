6f168bce1cd596cb445168c4ec585251cf049a7a
haily <tjsdn8963@naver.com>
2024-11-25 18:34:32 +0900

    241125 add board_view

--- myWeb/src/main/java/com/melly/myweb/board/BoardController.java
+++ myWeb/src/main/java/com/melly/myweb/board/BoardController.java
@@ -14,10 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,7 +26,7 @@ public class BoardController implements IResponseController {
     private IBoardService boardService;
 
     @GetMapping("/board_list")
-    public String boardAjxList(Model model) {
+    public String boardList(Model model) {
         try{
             makeResponseCheckLogin(model);
             List<BoardDto> list =  this.boardService.findAllByNameContains();
@@ -43,7 +40,7 @@ public class BoardController implements IResponseController {
         return "board/boardList";
     }
 
-    @GetMapping("/boardInsert_movement")
+    @GetMapping("/board_insert_page")
     public String boardInsert(Model model){
         try{
             makeResponseCheckLogin(model);
@@ -56,7 +53,7 @@ public class BoardController implements IResponseController {
         return "board/boardInsert";
     }
 
-    @PostMapping("/boardInsert")
+    @PostMapping("/board_insert")
     public String boardInsert(Model model,@ModelAttribute BoardDto boardDto){
         // @ModelAttribute CategoryDto dto : POST 방식의 요청은 값이 숨겨져 있다. HTTP Request web form
         //  :  주소에서 ?searchName=&page=값 변수의 값을 받는다. "application/x-www-form-?????"
@@ -69,5 +66,19 @@ public class BoardController implements IResponseController {
             log.error(ex.toString()); // error 응답
         }
         return "redirect:/board/board_list";
+    }
+
+    @GetMapping("/board_view")
+    public String boardView(Model model, @RequestParam Long id){
+        try{
+            CUDInfoDto cudInfoDto = makeResponseCheckLogin(model);
+            IBoard find  = this.boardService.findById(id);
+            model.addAttribute("boardView",find);
+        }catch (LoginAccessException ex){
+            log.error(ex.toString());
+        }catch (Exception ex){
+            log.error(ex.toString());
+        }
+        return "board/boardView";
     }
 }
