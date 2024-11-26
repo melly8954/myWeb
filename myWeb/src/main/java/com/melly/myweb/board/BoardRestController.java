06d10527af45c3b4fde8ac3280ca96cb4adebde6
haily <tjsdn8963@naver.com>
2024-11-26 22:25:29 +0900

    241126 게시판 좋아요 기능 미완성

--- myWeb/src/main/java/com/melly/myweb/board/BoardRestController.java
+++ myWeb/src/main/java/com/melly/myweb/board/BoardRestController.java
@@ -1,5 +1,7 @@
 package com.melly.myweb.board;
 
+import com.melly.myweb.board.like.BoardLikeDto;
+import com.melly.myweb.board.like.IBoardLikeMybatisMapper;
 import com.melly.myweb.commons.dto.CUDInfoDto;
 import com.melly.myweb.commons.dto.ResponseCode;
 import com.melly.myweb.commons.dto.ResponseDto;
@@ -8,6 +10,7 @@ import com.melly.myweb.commons.exception.IdNotFoundException;
 import com.melly.myweb.commons.exception.LoginAccessException;
 import com.melly.myweb.commons.inif.ICommonRestController;
 import com.melly.myweb.commons.inif.IResponseController;
+import com.melly.myweb.user.IUser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,6 +27,9 @@ import java.util.List;
 public class BoardRestController implements ICommonRestController<BoardDto> {
     @Autowired
     private IBoardService boardService;
+
+    @Autowired
+    private IBoardLikeMybatisMapper boardLikeMybatisMapper;
 
     @Override
     @PostMapping("boardInsert")
@@ -104,6 +110,63 @@ public class BoardRestController implements ICommonRestController<BoardDto> {
         }
     }
 
+    @GetMapping("/like/{id}")
+    public ResponseEntity<ResponseDto> addLikeQty(Model model, @Validated @PathVariable Long id){
+        try{
+            if(id == null || id <= 0){
+                return makeResponseEntity(HttpStatus.BAD_REQUEST,ResponseCode.R000051,"입력 매개변수 에러",null);
+            }
+            CUDInfoDto cudInfoDto = makeResponseCheckLogin(model);
+            this.boardService.addLikeQty(cudInfoDto,id);
+            IBoard result = this.getBoardAndLike(id,cudInfoDto.getLoginUser());
+            return makeResponseEntity(HttpStatus.OK,ResponseCode.R000000,"성공",result);
+        }catch (LoginAccessException ex){
+            log.error(ex.toString());
+            return makeResponseEntity(HttpStatus.FORBIDDEN,ResponseCode.R888881, ex.getMessage(),null);
+        }catch (IdNotFoundException ex){
+            log.error(ex.toString());
+            return makeResponseEntity(HttpStatus.NOT_FOUND,ResponseCode.R000041, ex.getMessage(),null);
+        }catch (Exception ex){
+            log.error(ex.toString());
+            return makeResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR,ResponseCode.R999999, ex.getMessage(),null);
+        }
+    }
+
+    @GetMapping("/unlike/{id}")
+    public ResponseEntity<ResponseDto> subLikeQty(Model model, @Validated @PathVariable Long id){
+        try{
+            if(id == null || id <= 0){
+                return makeResponseEntity(HttpStatus.BAD_REQUEST,ResponseCode.R000051,"입력 매개변수 에러",null);
+            }
+            CUDInfoDto cudInfoDto = makeResponseCheckLogin(model);
+            this.boardService.subLikeQty(cudInfoDto,id);
+            IBoard result = this.getBoardAndLike(id,cudInfoDto.getLoginUser());
+            return makeResponseEntity(HttpStatus.OK,ResponseCode.R000000,"성공",result);
+        }catch (LoginAccessException ex){
+            log.error(ex.toString());
+            return makeResponseEntity(HttpStatus.FORBIDDEN,ResponseCode.R888881, ex.getMessage(),null);
+        }catch (IdNotFoundException ex){
+            log.error(ex.toString());
+            return makeResponseEntity(HttpStatus.NOT_FOUND,ResponseCode.R000041, ex.getMessage(),null);
+        }catch (Exception ex){
+            log.error(ex.toString());
+            return makeResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR,ResponseCode.R999999, ex.getMessage(),null);
+        }
+    }
+
+
+    private IBoard getBoardAndLike(Long id, IUser loginUser){
+        IBoard result = this.boardService.findById(id);
+        BoardLikeDto boardLikeDto = BoardLikeDto.builder()
+                .tbl(new BoardDto().getTbl())
+                .createId(loginUser.getId())
+                .boardId(id)
+                .build();
+
+        Integer likeCount = this.boardLikeMybatisMapper.countByLike(boardLikeDto);
+        result.setDeleteDt(likeCount.toString());
+        return result;
+    }
     @Override
     public ResponseEntity<ResponseDto> update(Model model, Long id, BoardDto dto) {
         return null;
